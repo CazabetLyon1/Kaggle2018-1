12009806ad688c7d121503bc1099d458456907e0
PACCALIN AXEL p1709476 <axel.paccalin@etu.univ-lyon1.fr>
2018-09-21 09:28:24 +0200

    Adding first input formatter, flattens 3D array of images into 2D array of pixels

---
+++ InputFormatter.py
@@ -0,0 +1,8 @@
+
+
+# Flatten an array of n images to an array of n normalized vectors of pixels
+# images shape : (n, x, y)
+# output shape : (n, x*y)
+def to_normalized_vector_list(images):
+    pixels_qt = images.shape[1] * images.shape[2]  # Compute x*y
+    return images.reshape(images.shape[0], pixels_qt)/255
