74384237506c0bb9e899c6efb3c57ea26c663845
PACCALIN AXEL p1709476 <axel.paccalin@etu.univ-lyon1.fr>
2018-09-21 14:25:01 +0200

    Function converting an array of pixels vectors into an array of images

--- InputFormatter.py
+++ InputFormatter.py
@@ -1,7 +1,22 @@
+import math
+import numpy
+
 
 # Flatten an array of n images to an array of n normalized vectors of pixels
-# images shape : (n, x, y)
-# output shape : (n, x*y)
+# Images shape : (n, x, y,)
+# Output shape : (n, x*y,)
 def to_normalized_vector_list(images):
     pixels_qt = images.shape[1] * images.shape[2]          # Compute x*y
     return images.reshape(images.shape[0], pixels_qt)/255  # Flatten & nnormalize
+
+
+# Reconstitute an array of square image from an array of pixels vectors
+# Vectors shape : (n, x*y,)
+# Output  shape : (n, x, y,)
+def to_normalized_images(pixels_vectors):
+    i = math.sqrt(pixels_vectors.shape[1])
+    if i != int(i):
+        raise ValueError('pixels_vectors do not correspond to a square image')
+
+    i = int(i)
+    return numpy.array([pixels_vector.reshape(i, i) for pixels_vector in pixels_vectors])
